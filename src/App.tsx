065f84cd3c698f50e1edd296012cb3080e4afd55
Tohaprep <alehinant@mail.ru>
2025-06-30 10:17:33 +0500

    Deleted array

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { TodoList } from "./components/todolist";
 
-const tasks1 = [
-  { id: "0", title: "анжуманя", isDone: false },
-  { id: "1", title: "бегит", isDone: true },
-  { id: "2", title: "прес", isDone: true },
+const tasks2 = [
+  { id: "0", title: "монстры", isDone: true },
+  { id: "1", title: "тентасьон", isDone: false },
+  { id: "2", title: "кукиш", isDone: true },
 ];
 
 function App() {
